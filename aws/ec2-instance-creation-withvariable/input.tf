c1be1d19f0495dbfd4b132f43fbc8aec6ae98a37
asimops <arpanigrahi.1234@gmail.com>
2025-09-18 20:28:38 -0400

    Create input.tf

---
+++ aws/ec2-instance-creation-withvariable/input.tf
@@ -0,0 +1,12 @@
+//This step will define an input variable of the instance type. here we are trying to parameterised.
+variable instance_type {
+    description = "This is ec2 instance type"
+    type = string
+}
+
+
+//This step will define an input variable of the EC2 instance AMI ID
+variable ami_id {
+    description = "This is ec2 ami id"
+    type = string
+}
