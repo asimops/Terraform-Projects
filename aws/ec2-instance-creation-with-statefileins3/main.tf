a96394fb2c88df6fae65cf1761419c5318112139
asimops <arpanigrahi.1234@gmail.com>
2025-09-23 00:46:56 -0400

    Update main.tf

--- aws/ec2-instance-creation-with-statefileins3/main.tf
+++ aws/ec2-instance-creation-with-statefileins3/main.tf
@@ -7,3 +7,13 @@ resource "aws_instance" "hello-world" {
 resource "aws_s3_bucket" "s3_bucket" {
      bucket = var.s3
 }
+
+//To lock the state file you have to create the dynamodb table and store with lockid. here table name is terraform-lock
+resource "aws_dynamodb_table" "terraform_lock" {
+  name           = "terraform-lock"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
