4ff383d5368bd3ceec3a518221ff33a1e637e7bd
asimops <arpanigrahi.1234@gmail.com>
2025-09-23 00:41:01 -0400

    Create output.tf

---
+++ aws/ec2-instance-creation-with-statefileins3/output.tf
@@ -0,0 +1,5 @@
+//This step will define an outpt variable which will show the output variable of the ec2 instance
+output "public_ip" {
+description = "Public IP address of the ec2 instance"
+value = aws_instance.hello-world.public_ip
+}
